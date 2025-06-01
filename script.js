267dbedda3492842530ba5856ccd80c3699d6b37
vid981 <dth3315@gmail.com>
2025-06-01 19:06:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+
+const openingCard = document.getElementById("openingCard");
+const page1Text = document.getElementById("page1Text");
+const pages = document.querySelectorAll(".page");
+let currentPage = 1;
+
+openingCard.addEventListener("click", () => {
+  let messages = [
+    "Happy birthday Madamji !!!",
+    "It’s your special day",
+    "So..........!!",
+    "I want to make something special for you"
+  ];
+  let index = 0;
+  page1Text.style.opacity = 0;
+
+  const showNext = () => {
+    if (index < messages.length) {
+      page1Text.textContent = messages[index];
+      page1Text.style.opacity = 1;
+      setTimeout(() => {
+        page1Text.style.opacity = 0;
+        index++;
+        setTimeout(showNext, 600);
+      }, 2000);
+    } else {
+      setTimeout(() => {
+        openingCard.style.opacity = 0;
+        setTimeout(() => {
+          goToPage(2);
+        }, 800);
+      }, 500);
+    }
+  };
+
+  showNext();
+});
+
+function goToPage(n) {
+  pages.forEach(p => p.classList.remove("active"));
+  document.getElementById(`page${n}`).classList.add("active");
+  currentPage = n;
+}
+
+const audio = document.getElementById("voiceNote");
+const toPage3Btn = document.getElementById("toPage3");
+audio.addEventListener("ended", () => {
+  toPage3Btn.classList.remove("hidden");
+});
+toPage3Btn.addEventListener("click", () => goToPage(3));
+
+let flippedCount = 0;
+const cards = document.querySelectorAll(".flip-card");
+const toPage4Btn = document.getElementById("toPage4");
+
+cards.forEach(card => {
+  let flipped = false;
+  card.addEventListener("click", () => {
+    if (!flipped) {
+      card.classList.add("flipped");
+      card.textContent = card.getAttribute("data-text");
+      flipped = true;
+      flippedCount++;
+      if (flippedCount === cards.length) {
+        toPage4Btn.classList.remove("hidden");
+      }
+    }
+  });
+});
+toPage4Btn.addEventListener("click", () => goToPage(4));
